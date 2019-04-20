319191f711aaa557a6ae9e202c54330867ba2a16
李豪作 <lihaozuo@udesk.cn>
2019-04-20 15:40:51 +0800

    added result_handler to handle http api return

--- lib/apollo_client.rb
+++ lib/apollo_client.rb
@@ -1,11 +1,26 @@
 class ApolloClient
 
-  def get_value
-    
+  def initialize(app_id="", cluster_name = "default", name_space = "appilication")
+    @app_id = app_id
+    @cluster_name = cluster_name
+    @name_space = name_space
+  end
+
+  def get(key = "")
+    map(key)
+  end
+
+  def map
+    http_get(default_url)
   end
 
   def http_get(url, headers={})
     resuorce = RestClient::Resource.new(url, timeout: 15, open_timeout: 15)
-    load_json(resuorce.get(:params => headers))
+    JSON.parse(resuorce.get(:params => headers))
+  end
+
+  def default_url()
+    url = "http://localhost:8080/configfiles/json/SampleApp/default/application"
+    url
   end
 end
